398dc620b881d5a7c467a1a68d67819267e5378c
stepin104887 <69084828+stepin104887@users.noreply.github.com>
2020-09-19 06:05:24 +0530

    Create calculator_operations.h

---
+++ inc/calculator_operations.h
@@ -0,0 +1,31 @@
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+#define PI 3.14159265
+
+
+float sine(float x);
+float cosine(float x);
+float tangent(float x);
+float cosec(float x);
+float sec(float x);
+float cot(float x);
+float logten(float x);
+float squareroot(float x);
+float cuberoot(float x);
+float exponent(float x);
+float power(float x, float y);
+float addition(int arr[],int x);
+float subtraction(float x, float y);
+float multiplication(int arr[],int x);
+float division(float x, float y);
+float modulo(float x, float y);
+
+
+
+
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
+
